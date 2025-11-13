41ffc9662ac4967ff0a147f6bd8324c4235f7c08
cthomas <caren@letta.com>
2025-11-13 15:26:06 -0800

    fix: revert lettuce client conditional import (#6163)

--- letta/services/lettuce/__init__.py
+++ letta/services/lettuce/__init__.py
@@ -1,3 +1,6 @@
-from .lettuce_client import LettuceClient
+try:
+    from .lettuce_client import LettuceClient
+except ImportError:
+    from .lettuce_client_base import LettuceClient
 
 __all__ = ["LettuceClient"]
